1b56c3db460ec53e26cd3e2a7c0b0dfbfd54df55
kofhb <117967016+kofhb@users.noreply.github.com>
2022-11-29 19:37:05 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -11,8 +11,8 @@ CACHE_TIME = 300
 USE_CAPTION_FILTER = True
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 98765432]
-CHANNELS = [-10012345678, -100987654321, 'channelusername']
+ADMINS = [123457, 'admin', 123]
+CHANNELS = [-10012345678, -100987, 'channelusername']
 AUTH_USERS = []
 AUTH_CHANNEL = None
 
